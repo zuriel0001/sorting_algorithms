7c78d49d144aa18171256f75057d73ea0c4a2357
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-07-22 12:42:46 -0700

    count array

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -20,3 +20,49 @@ int get_max(int *array, int size)
 	return (m);
 }
 
+/**
+ * arrange_sort - Sort an array of integers in ascending order using
+ * the counting sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the counting array after setting it up.
+ */
+void arrange_sort(int *array, size_t size)
+{
+	int *my_count, *sort, maximum, i;
+
+	if (array == NULL || size < 2)
+		return;
+
+	sort = malloc(sizeof(int) * size);
+	if (sort == NULL)
+		return;
+	maximum = get_max(array, size);
+	my_count = malloc(sizeof(int) * (max + 1));
+	if (my_count == NULL)
+	{
+		free(sort);
+		return;
+	}
+
+	for (i = 0; i < (maximum + 1); i++)
+		my_count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		my_count[array[i]] += 1;
+	for (i = 0; i < (maximum + 1); i++)
+		count[i] += count[i - 1];
+	print_array(count, max + 1);
+
+	for (i = 0; i < (int)size; i++)
+	{
+		sorted[count[array[i]] - 1] = array[i];
+		count[array[i]] -= 1;
+	}
+
+	for (i = 0; i < (int)size; i++)
+		array[i] = sorted[i];
+
+	free(sorted);
+	free(count);
+}
