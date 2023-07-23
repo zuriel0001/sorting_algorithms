675ad2d1264303eecce4e27d4c1688013dc4bd63
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-07-23 11:44:55 -0700

    merge sort

---
+++ 103-merge_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+void merge_subarray(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back);
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+void merge_sort(int *array, size_t size);
+
+/**
+ * merge_subarray - Function thar Sort a subarray of integers.
+ * @subarr: A subarray of an array of integers to sort.
+ * @buff: A buffer to store the sorted subarray.
+ * @front: front index of the array.
+ * @mid:  middle index of the array.
+ * @back: back index of the array.
+ */
+void merge_subarray(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back)
+{
+	size_t a, b, c = 0;
+
+	printf("Merging...\n[left]: ");
+	print_array(subarr + front, mid - front);
+
+	printf("[right]: ");
+	print_array(subarr + mid, back - mid);
+
+	for (a = front, b = mid; a < mid && b < back; c++)
+		buff[c] = (subarr[a] < subarr[b]) ? subarr[a++] : subarr[b++];
+	for (; a < mid; a++)
+		buff[c++] = subarr[a];
+	for (; b < back; b++)
+		buff[c++] = subarr[b];
+	for (a = front, c = 0; a < back; a++)
+		subarr[a] = buff[c++];
+
+	printf("[Done]: ");
+	print_array(subarr + front, back - front);
+}
+
